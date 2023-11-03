ee7809d50b98d4623e9f89c8faabdad1b2572ed5
Sofia <norinasofia@mail.ru>
2023-11-03 12:21:58 +0300

    Initial Commit

---
+++ MNR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MNR
+//
+//  Created by Софья Норина on 3.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
